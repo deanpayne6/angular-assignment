34f1e591213a080ad8479c5c488a7ac72bac06dd
Dean Payne <deandeanpayne@gmail.com>
2023-04-10 18:05:08 -0700

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts     Removed the old vanilla html date picker JS code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,40 +15,4 @@ export class AppComponent {
     hiringManagerName: new FormControl(''),
     hiringManagerEmail: new FormControl('')
   })
-  
-  
-  //getting the current date to set as the MIN value of the date selector
-  TodayDate : any;
-  minDate : any;
-  date1 = new Date();
-
-  // variables to store current year, month, day
-  currentYear = this.date1.getUTCFullYear();
-  currentMonth = this.date1.getUTCMonth() + 1;
-  currentDay = this.date1.getUTCDate() - 1;
-
-  FinalMonth: any;
-  FinalDay: any;
-
-  date = new FormControl('');
-  
-
-  //initializing to format todays current date correctly
-  ngOnInit() : void {
-    if(this.currentMonth < 10){
-      this.FinalMonth = "0" + this.currentMonth;
-    }else{
-      this.FinalMonth = this.currentMonth;
-    }
-
-    if(this.currentDay < 10){
-      this.FinalDay ="0" + this.currentDay;
-    }else{
-      this.FinalDay = this.currentDay;
-    }
-
-    this.TodayDate = this.currentYear + "-" + this.FinalMonth + "-" + this.FinalDay;
-    console.log(this.TodayDate);
-    this.minDate = this.TodayDate;
-  }
 }
